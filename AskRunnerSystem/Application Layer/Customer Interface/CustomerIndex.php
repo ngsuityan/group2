349753d51a6eac008140a27469f271e6925fd4d7
SohXinJie <67514802+SohXinJie@users.noreply.github.com>
2021-06-03 02:23:31 +0800

    Update CustomerIndex.php
    Show price in the Pet Assist Product Page

--- AskRunnerSystem/Application Layer/Customer Interface/CustomerIndex.php
+++ AskRunnerSystem/Application Layer/Customer Interface/CustomerIndex.php
@@ -101,7 +101,8 @@
              <div class="row">
                 <div class="col-md-3 kotakkedai text-center py-4 m-4">
                     <a href="CustomerIndex.php?type=pet&proid=<?php echo $proid?>">
-                        <legend><h1><?php echo $row['PET_NAME'];?></h1></legend>
+                        <legend><h1><?php echo $row['PET_NAME'];?><?php echo "<br> RM ";?>  
+                            <?php echo $row['PET_PRICE'];?></h1></legend>
                         <img src="../IMG/<?php echo $row['PET_IMAGE'];?>"   style="width: 100%;height: 70%;margin-top: 10px;border-radius: 10px;">
                     <button class="btn btn-info button2 btn-block mt-2"><h4>View</h4></button>
                     </a>
